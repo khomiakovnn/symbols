7f462e34309bb7e7685c30efaf027fe3a503eccc
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-10-02 17:35:02 -0400

    done

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,7 +1,24 @@
-import Character from "./Character";
-
 export default class Team {
   constructor() {
     this.list = [];
+  }
+
+  [Symbol.iterator]() {
+    let current = 0;
+    const last = this.list.length - 1;
+    const value = this.list[current += 1];
+    return {
+      next: () => {
+        if (current <= last) {
+          return {
+            done: false,
+            value,
+          };
+        }
+        return {
+          done: true,
+        };
+      },
+    };
   }
 }
